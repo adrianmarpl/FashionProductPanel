71de6ad87cb88376f769602bab94c055a21d51c7
Adek <adrian.marjanski@gmail.com>
2024-02-05 15:48:03 +0100

    fix

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -58,23 +58,43 @@ const CheckboxList: React.FC<CheckboxListProps> = ({ name, dataList, value, setV
     )
 }
 
+const ProductInfo = ({ product }) => {
+    return (
+        <div className="col-12 md:col-4">
+            <a href={product.link} target="_blank" rel="noreferrer">
+                <div className="card">
+                    <img src={product.image} alt={product.name} />
+                    <h5>{product.name}</h5>
+                    <p>{product.description}</p>
+                </div>
+            </a>
+        </div>
+    )
+}
+
 const Dashboard = () => {
     const skirtList = ['mini', 'przed kolano', 'midi', 'maxi'];
-    const stylesList = ['boho','glamour','vintige','rockowy','smart casual','minimalistyczny','romantyczny','sportowy'];
-    const patternList = ['kwiatki','groszki','napisy','logo','moro','zwierzęce','pepitka','kratka','paski']; //,'wszystkie lubię'
+    const stylesList = ['boho', 'glamour', 'vintige', 'rockowy', 'smart casual', 'minimalistyczny', 'romantyczny', 'sportowy'];
+    const patternList = ['kwiatki', 'groszki', 'napisy', 'logo', 'moro', 'zwierzęce', 'pepitka', 'kratka', 'paski']; //,'wszystkie lubię'
     const pantsList = ['do kostki', 'regular', 'długie'];
 
     const [typeClothes, setTypeClothes] = useState('mix');
     const [skirtLength, setSkirtLength] = useState<string[]>(skirtList);
     const [patterns, setPatterns] = useState<string[]>(patternList);
     const [styles, setStyles] = useState<string[]>(stylesList);
-    const [colors, setColors] = useState<string[]>(['czerwony','zielony','biały']);
+    const [colors, setColors] = useState<string[]>(['czerwony', 'zielony', 'biały']);
     const [fashion, setFashion] = useState('regular');
     const [pantsLength, setPantsLength] = useState<string[]>(pantsList);
 
+    const [products, setProducts] = useState([]);
+    const [products2, setProducts2] = useState([]);
+    const [products3, setProducts3] = useState([]);
+
+    const [generateIndex, setGenerateIndex] = useState(0);
 
 
     const generate = () => {
+        setGenerateIndex(0);
         console.log('typeClothes', typeClothes);
         console.log('skirtLength', skirtLength);
         console.log('patterns', patterns);
@@ -82,7 +102,7 @@ const Dashboard = () => {
         console.log('fashion', fashion);
         console.log('pantsLength', pantsLength);
         const data = {
-            typeClothes: typeClothes=="mix"?['spodnie','sukienka']:[typeClothes],
+            typeClothes: typeClothes == "mix" ? ['spodnie', 'sukienka'] : [typeClothes],
             skirtLength: skirtLength,
             patterns: patterns,
             styles: styles,
@@ -97,11 +117,18 @@ const Dashboard = () => {
             },
             body: JSON.stringify(data),
         }).then(response => response.json())
-        // .then(data => console.log('resp: ',data))
-        .catch((error) => console.error('Error:', error));
+            .catch((error) => console.error('Error:', error));
         generateProducts.then((data) => {
             console.log('products: ', data);
+            setProducts(data.result1);
+            setProducts2(data.result2);
+            setProducts3(data.result3);
         });
+    }
+
+    const generateNext = () => {
+        generate();
+        //setGenerateIndex(generateIndex + 1);
     }
 
     useEffect(() => {
@@ -109,50 +136,68 @@ const Dashboard = () => {
 
 
     return (
-        <div className="grid">
-            <div className="col-12">
-                <div className="card">
-                    <h5>Rodzaj ubrań</h5>
-                    <div className="grid">
-                        <RadioButtonList name="typeClothes" dataList={['spodnie', 'sukienka', 'mix']} value={typeClothes} setValue={setTypeClothes} />
+        <>
+            {products && products.length == 0 && <div className="grid">
+                <div className="col-12">
+                    <div className="card">
+                        <h5>Rodzaj ubrań</h5>
+                        <div className="grid">
+                            <RadioButtonList name="typeClothes" dataList={['spodnie', 'sukienka', 'mix']} value={typeClothes} setValue={setTypeClothes} />
+                        </div>
+                    </div>
+                    <div className="card">
+                        <h5>Preferowana długość spódnicy</h5>
+                        <div className="grid">
+                            <CheckboxList name="skirtLength" dataList={skirtList} value={skirtLength} setValue={setSkirtLength} />
+                        </div>
+                    </div>
+                    <div className="card">
+                        <h5>Wzory których nie chcesz nosić</h5>
+                        <div className="grid">
+                            <CheckboxList name="patterns" dataList={patternList} value={patterns} setValue={setPatterns} />
+                        </div>
+                    </div>
+                    <div className="card">
+                        <h5>Styl, który jest Tobie najbliższy</h5>
+                        <div className="grid">
+                            <CheckboxList name="styles" dataList={stylesList} value={styles} setValue={setStyles} />
+                        </div>
+                    </div>
+                    <div className="card">
+                        <h5>Wybierz swój fason</h5>
+                        <div className="grid">
+                            <RadioButtonList name="fashion" dataList={['petit', 'regular', 'tall', 'plus size', 'over size']} value={fashion} setValue={setFashion} />
+                        </div>
+                    </div>
+                    <div className="card">
+                        <h5>Spodnie z jaką długością nogawek preferujesz</h5>
+                        <div className="grid">
+                            <CheckboxList name="pantsLength" dataList={pantsList} value={pantsLength} setValue={setPantsLength} />
+                        </div>
                     </div>
                 </div>
-                <div className="card">
-                    <h5>Preferowana długość spódnicy</h5>
-                    <div className="grid">
-                        <CheckboxList name="skirtLength" dataList={skirtList} value={skirtLength} setValue={setSkirtLength} />
+                <div className='floating-button'>
+                    <Button label="Wygeneruj" rounded onClick={(e) => generate()} />
+                </div>
+            </div>}
+            {products && products.length > 0 &&
+                <div className="grid">
+                    {products.length > generateIndex+1 &&
+                        <ProductInfo product={products[generateIndex]}></ProductInfo>
+                    }
+                    {products2.length > generateIndex+1 &&
+                        <ProductInfo product={products2[generateIndex]}></ProductInfo>
+                    }
+                    {products3.length > generateIndex+1 &&
+                        <ProductInfo product={products3[generateIndex]}></ProductInfo>
+                    }
+
+                    <div className='floating-button'>
+                        <Button label="Następna" rounded onClick={(e) => generateNext()} />
                     </div>
                 </div>
-                <div className="card">
-                    <h5>Wzory których nie chcesz nosić</h5>
-                    <div className="grid">
-                        <CheckboxList name="patterns" dataList={patternList} value={patterns} setValue={setPatterns} />
-                    </div>
-                </div>
-                <div className="card">
-                    <h5>Styl, który jest Tobie najbliższy</h5>
-                    <div className="grid">
-                        <CheckboxList name="styles" dataList={stylesList} value={styles} setValue={setStyles} />
-                    </div>
-                </div>
-                <div className="card">
-                    <h5>Wybierz swój fason</h5>
-                    <div className="grid">
-                        <RadioButtonList name="fashion" dataList={['petit','regular','tall','plus size','over size']} value={fashion} setValue={setFashion} />
-                    </div>
-                </div>
-                {/* kolory */}
-                <div className="card">
-                    <h5>Spodnie z jaką długością nogawek preferujesz</h5>
-                    <div className="grid">
-                        <CheckboxList name="pantsLength" dataList={pantsList} value={pantsLength} setValue={setPantsLength} />
-                    </div>
-                </div>
-            </div>
-            <div className='floating-button'>
-                <Button label="Wygeneruj" rounded onClick={(e)=>generate()} />
-            </div>
-        </div>
+            }
+        </>
     );
 };
 
